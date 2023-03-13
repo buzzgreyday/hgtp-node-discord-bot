069ded664073a36be099cd03e27d0596a4f39f43
Michael <micbr@pm.me>
2023-03-13 21:58:40 +0100

    Minor change

--- functions/process.py
+++ functions/process.py
@@ -12,12 +12,13 @@ async def online_node_cluster(dask_client, node_data, cluster_data):
     lb_ids = await dask_client.compute(load_balancers["id"].values)
     for d in cluster_data:
         for k, v in d.items():
-            if k == "ip":
-                pair_ip = str(v)
-            if k == "publicPort":
-                pair_port = str(v)
             if (k == "id") and (str(v) in lb_ids):
                 node_cluster_name = await dask_client.compute(load_balancers["name"][load_balancers["id"] == str(v)])
                 node_data["nodeClusterName"] = node_cluster_name.values[0]
+            if k == "ip":
+                pair_ip = str(v)
                 node_data["nodeClusterIp"] = pair_ip
+            if k == "publicPort":
+                pair_port = str(v)
                 node_data["nodeClusterPublicPort"] = pair_port
+
