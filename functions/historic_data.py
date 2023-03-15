ed59f10f1057a7f2432a004b04f7607b89e683da
Michael <micbr@pm.me>
2023-03-15 22:19:20 +0100

    Bgun writing merge history data

--- functions/historic_data.py
+++ functions/historic_data.py
@@ -15,19 +15,26 @@ async def node_data(dask_client, node_data, history_dataframe):
 
 async def former_data(node_data, historic_node_data):
     # STD/OFFLINE VALUES
-    former_tessellation_version = 0
-    former_connectivity = None
+    former_node_id = []
+    former_node_connectivity = []
+    former_node_wallet = []
+    former_node_tessellation_version = []
+    former_node_cluster_association_time = []
+    former_node_cluster_dissociation_time = []
+    former_node_total_disk_space = []
+    former_node_free_disk_space = []
 
     former_node_data = historic_node_data[historic_node_data["index timestamp"] == historic_node_data["index timestamp"].max()]
     if not former_node_data.empty:
         former_cluster_names = list(set(former_node_data["cluster name"]))
         for cluster_name in former_cluster_names:
-            former_node_id = former_node_data["node id"][former_node_data["cluster name"] == cluster_name].values[0]
-            former_node_connectivity = former_node_data["connectivity"][former_node_data["cluster name"] == cluster_name].values[0]
-            former_node_wallet = former_node_data["node wallet"][former_node_data["cluster name"] == cluster_name].values[0]
-            former_node_tessellation_version = former_node_data["node version"][former_node_data["cluster name"] == cluster_name].values[0]
-            former_node_cluster_association_time = former_node_data["association time"][former_node_data["cluster name"] == cluster_name].values[0]
-            former_node_cluster_dissociation_time = former_node_data["dissociation time"][former_node_data["cluster name"] == cluster_name].values[0]
-            former_node_total_disk_space = former_node_data["node total disk space"][former_node_data["cluster name"] == cluster_name].values[0]
-            former_node_free_disk_space = former_node_data["node free disk space"][former_node_data["cluster name"] == cluster_name].values[0]
+            former_node_id.append(former_node_data["node id"][former_node_data["cluster name"] == cluster_name].values[0])
+            # former_cluster_name.append(former_node_data["cluster name"][former_node_data["cluster name"] == cluster_name].values[0])
+            former_node_connectivity.append(former_node_data["connectivity"][former_node_data["cluster name"] == cluster_name].values[0])
+            former_node_wallet.append(former_node_data["node wallet"][former_node_data["cluster name"] == cluster_name].values[0])
+            former_node_tessellation_version.append(former_node_data["node version"][former_node_data["cluster name"] == cluster_name].values[0])
+            former_node_cluster_association_time.append(former_node_data["association time"][former_node_data["cluster name"] == cluster_name].values[0])
+            former_node_cluster_dissociation_time.append(former_node_data["dissociation time"][former_node_data["cluster name"] == cluster_name].values[0])
+            former_node_total_disk_space.append(former_node_data["node total disk space"][former_node_data["cluster name"] == cluster_name].values[0])
+            former_node_free_disk_space.append(former_node_data["node free disk space"][former_node_data["cluster name"] == cluster_name].values[0])
 
