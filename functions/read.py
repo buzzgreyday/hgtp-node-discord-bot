bf6a4853ec51f485392efe92fa18b9df1dd7b319
Michael <micbr@pm.me>
2023-03-18 18:11:22 +0100

    refactored config.yaml

--- functions/read.py
+++ functions/read.py
@@ -6,12 +6,12 @@ import dask.dataframe as dd
 
 
 async def history(configuration):
-    if not os.path.exists(configuration["file locations"]["history"]):
+    if not os.path.exists(configuration["file settings"]["locations"]["history"]):
         logging.warning(f"{datetime.utcnow().strftime('%H:%M:%S')} - NODE DATA NOT FOUND, RETURN BLANK DATAFRAME WITH COLUMNS")
-        return pd.DataFrame(columns=configuration["file locations"]["columns"]["history"])
-    elif os.path.exists(configuration["file locations"]["history"]):
+        return pd.DataFrame(columns=configuration["file settings"]["columns"]["history"])
+    elif os.path.exists(configuration["file settings"]["locations"]["history"]):
         logging.info(f"{datetime.utcnow().strftime('%H:%M:%S')} - NODE DATA FOUND, RETURN READ DATAFRAME")
-        return dd.read_parquet(configuration["file locations"]["history"], columns=configuration["file locations"]["columns"]["history"])
+        return dd.read_parquet(configuration["file settings"]["locations"]["history"], columns=configuration["file settings"]["columns"]["history"])
     else:
         logging.critical(f"{datetime.utcnow().strftime('%H:%M:%S')} - SOMETHING WENT WRONG WHILE ATTEMPTING TO FIND NODE DATA")
         return pd.DataFrame()
@@ -19,9 +19,4 @@ async def history(configuration):
 
 async def subscribers(configuration):
     logging.info(f"{datetime.utcnow().strftime('%H:%M:%S')} - READING SUBSCRIBER DATA AND RETURNING DATAFRAME")
-    return dd.read_csv(f'{configuration["file locations"]["subscribers"]}', dtype=configuration["file locations"]["dtypes"]["subscribers"])
-
-
-async def load_balancers(configuration):
-    logging.info(f"{datetime.utcnow().strftime('%H:%M:%S')} - READING LOAD BALANCER DATA AND RETURNING DATAFRAME")
-    return dd.read_csv(f'{configuration["file locations"]["load balancers"]}', dtype=configuration["file locations"]["dtypes"]["subscribers"])
+    return dd.read_csv(f'{configuration["file settings"]["locations"]["subscribers"]}', dtype=configuration["file settings"]["dtypes"]["subscribers"])
