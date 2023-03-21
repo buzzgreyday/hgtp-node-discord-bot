085b752b96482397686cb21d3b5b0ba2aaf0346b
Michael <micbr@pm.me>
2023-03-21 08:34:22 +0100

    Fixed all known issues

--- functions/historic_data.py
+++ functions/historic_data.py
@@ -27,6 +27,7 @@ async def merge_node_data(node_data: dict, historic_node_dataframe) -> dict:
             self._value = value
 
         def make_lower(self) -> str | None:
+            self._value = Clean(self._value).make_none()
             if self._value is not None:
                 return self._value.lower()
         def make_none(self) -> str | None:
@@ -39,14 +40,14 @@ async def merge_node_data(node_data: dict, historic_node_dataframe) -> dict:
     """IF HISTORIC DATA EXISTS"""
     if not historic_node_dataframe.empty:
         # node_data["formerClusterNames"] = str(historic_node_dataframe["cluster name"].values[0])
-        node_data["formerClusterNames"] = Clean(historic_node_dataframe["cluster name"].values[0]).make_lower()
+        node_data["formerClusterNames"] = Clean(historic_node_dataframe["cluster name"]).make_lower()
         node_data["formerClusterConnectivity"] = Clean(historic_node_dataframe["connectivity"][historic_node_dataframe["cluster name"] == node_data["formerClusterNames"]]).make_none()
         node_data["formerClusterAssociationTime"] = Clean(historic_node_dataframe["association time"][historic_node_dataframe["cluster name"] == node_data["formerClusterNames"]]).make_none()
         node_data["formerClusterDissociationTime"] = Clean(historic_node_dataframe["dissociation time"][historic_node_dataframe["cluster name"] == node_data["formerClusterNames"]]).make_none()
         if node_data["state"] == "Offline":
-            node_data["id"] = str(historic_node_dataframe["node id"].values[0])
-            node_data["nodeWalletAddress"] = str(historic_node_dataframe["node wallet"].values[0])
-            node_data["version"] = str(historic_node_dataframe["node version"].values[0])
+            node_data["id"] = Clean(historic_node_dataframe["node id"]).make_none()
+            node_data["nodeWalletAddress"] = Clean(historic_node_dataframe["node wallet"]).make_none()
+            node_data["version"] = Clean(historic_node_dataframe["node version"]).make_none()
             node_data["diskSpaceTotal"] = float(historic_node_dataframe["node total disk space"])
             node_data["diskSpaceFree"] = float(historic_node_dataframe["node free disk space"])
     return node_data
