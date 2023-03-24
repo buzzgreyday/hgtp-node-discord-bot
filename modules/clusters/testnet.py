a1bdcfa67f1be90aef5db157dfb65680f4443e8b
Michael <micbr@pm.me>
2023-03-25 00:07:32 +0100

    Did some serious refactoring: more simplicity

--- modules/clusters/testnet.py
+++ modules/clusters/testnet.py
@@ -159,10 +159,7 @@ async def safe_request(request_url: str, configuration: dict):
 
             else:
                 data = await Request(request_url).json(configuration)
-            if retry_count >= configuration['request']['max retry count']:
-                data = None
-                break
-            elif data == 503:
+            if retry_count >= configuration['request']['max retry count'] | data == 503:
                 data = None
                 break
             elif data is not None:
