558bd46576540a592d1e900d794112283ce61141
Michael <micbr@pm.me>
2023-03-26 00:59:21 +0100

    Refactoring: moved Class

--- modules/merge.py
+++ modules/merge.py
@@ -1,18 +1,19 @@
+class Clean:
+    def __init__(self, value):
+        self._value = value
+
+    def make_lower(self) -> str | None:
+        self._value = Clean(self._value).make_none()
+        if self._value is not None:
+            return self._value.lower()
+
+    def make_none(self) -> str | None:
+        if len(self._value) != 0:
+            return self._value.values[0]
+        else:
+            return None
+
 async def historic_data(node_data: dict, historic_node_dataframe) -> dict:
-    class Clean:
-        def __init__(self, value):
-            self._value = value
-
-        def make_lower(self) -> str | None:
-            self._value = Clean(self._value).make_none()
-            if self._value is not None:
-                return self._value.lower()
-        def make_none(self) -> str | None:
-            if len(self._value) != 0:
-                return self._value.values[0]
-            else:
-                return None
-
 
     """IF HISTORIC DATA EXISTS"""
     if not historic_node_dataframe.empty:
