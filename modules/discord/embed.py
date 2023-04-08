7b86c9608fe8b7c6c068febbabd4709c93874f4e
Michael <micbr@pm.me>
2023-04-08 12:22:34 +0200

    Create embeds general

--- modules/discord/embed.py
+++ modules/discord/embed.py
@@ -22,42 +22,42 @@ def build_title(node_data):
 
 def build_general(node_data):
     if node_data["state"] != "offline" and node_data['id'] is not None:
-        general_node_state = f"**Node**\n" \
+        general_node_state = f"**NODE**\n" \
                              f":green_square: Online\n" \
                              f"```" \
                              f"Id: {node_data['id'][:6]}...{node_data['id'][-6:]}\n" \
                              f"Ip: {node_data['host']}\n" \
                              f"Port: {node_data['publicPort']}```"
     elif node_data["state"] != "offline" and node_data['id'] is None:
-        general_node_state = f"**Node**\n" \
+        general_node_state = f"**NODE**\n" \
                              f":green_square: Online\n" \
                              f"```" \
                              f"Ip: {node_data['host']}\n" \
                              f"Port: {node_data['publicPort']}```"
     elif node_data["state"] == "offline" and node_data['id'] is not None:
-        general_node_state =f"**Node**\n" \
+        general_node_state =f"**NODE**\n" \
                              f":red_square: Offline\n" \
                              f"```" \
                              f"Id: {node_data['id'][:6]}...{node_data['id'][-6:]}\n" \
                              f"Ip: {node_data['host']}\n" \
                              f"Port: {node_data['publicPort']}```"
     elif node_data["state"] == "offline" and node_data['id'] is None:
-        general_node_state = f"**Node**\n" \
+        general_node_state = f"**NODE**\n" \
                              f":red_square: Offline\n" \
                              f"```" \
                              f"Ip: {node_data['host']}\n" \
                              f"Port: {node_data['publicPort']}```"
 
     if node_data["clusterConnectivity"] in ("new association", "associated"):
-        general_cluster_connectivity = f"**Cluster**\n" \
+        general_cluster_connectivity = f"**CLUSTER**\n" \
                                        f":green_square: {str(node_data['clusterConnectivity']).title()}\n" \
                                        f"```{str(node_data['clusterNames']).title()}```"
     elif node_data["clusterConnectivity"] in ("new dissociation", "dissociated"):
-        general_cluster_connectivity = f"**Cluster**\n" \
+        general_cluster_connectivity = f"**CLUSTER**\n" \
                                        f":red_square: {str(node_data['clusterConnectivity']).title()}\n" \
                                        f"```{str(node_data['formerClusterNames']).title()}```"
     else:
-        general_cluster_connectivity = f"**Cluster**\n" \
+        general_cluster_connectivity = f"**CLUSTER**\n" \
                                        f":yellow_square: Unknown cluster"
 
     return general_node_state, general_cluster_connectivity
@@ -68,6 +68,6 @@ def build_embed(node_data):
     title = build_title(node_data)
     general_node_state, general_cluster_connectivity = build_general(node_data)
     embed = nextcord.Embed(title=title)
-    embed.add_field(name="GENERAL", value=general_node_state)
+    embed.add_field(name="\u200B", value=general_node_state)
     embed.add_field(name=f"\u200B", value=general_cluster_connectivity)
     return embed
