94f83f300b0bd80dacff1346381cd7432d650a34
Michael <micbr@pm.me>
2023-04-22 09:18:29 +0200

    Refactored info to use symbols instead of emojis and added days + percent
    Fixed reward check

--- modules/clusters/testnet.py
+++ modules/clusters/testnet.py
@@ -148,8 +148,10 @@ def reward_check(node_data: dict, all_supported_clusters_data: list):
             # if (cluster["cluster name"] == node_data["clusterNames"]) or (cluster["cluster name"] == node_data["formerClusterNames"]):
             if str(node_data["nodeWalletAddress"]) in cluster["recently rewarded"]:
                 node_data["rewardState"] = True
-            elif (cluster["recently rewarded"] is None) and (str(node_data["nodeWalletAddress"]) not in cluster["recently rewarded"]):
+                break
+            elif str(node_data["nodeWalletAddress"]) not in cluster["recently rewarded"]:
                     node_data["rewardState"] = False
+        break
 
     return node_data
 
