1d033bce0738764fd6e2f9b7f0140af43d6b3daf
Michael <micbr@pm.me>
2023-06-18 12:18:10 +0200

    Fix set time_difference

--- assets/code/user.py
+++ assets/code/user.py
@@ -24,7 +24,6 @@ async def check(dask_client, latest_tessellation_version, requester, subscriber_
     for id_ in await locate_ids(dask_client, requester, subscriber_dataframe):
         subscriber = await locate_node(dask_client, subscriber_dataframe, id_)
         for L in list(set(subscriber["layer"])):
-            print(L)
             for port in list(set(subscriber.public_port[subscriber.layer == L])):
                 futures.append(asyncio.create_task(
                     node.check(dask_client, bot, process_msg, requester, subscriber, port, L,
