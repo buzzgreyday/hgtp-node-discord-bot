029fc699bb9ac5c06df0e2607560bcf98467da9b
Michael <micbr@pm.me>
2023-06-18 12:25:16 +0200

    Removed old data template

--- assets/code/node.py
+++ assets/code/node.py
@@ -22,52 +22,6 @@ def merge_data(node_data: schemas.Node, cluster_data):
     return node_data
 
 
-def data_template(requester, subscriber, port: int, layer: int, latest_tessellation_version: str, dt_start):
-    return {
-        "name": subscriber["name"][subscriber.public_port == port].values[0],
-        "contact": subscriber["contact"][subscriber.public_port == port].values[0],
-        "host": subscriber["ip"][subscriber.public_port == port].values[0],
-        "layer": layer,
-        "publicPort": port,
-        "p2pPort": None,
-        "id": subscriber["id"][subscriber.public_port == port].values[0],
-        "nodeWalletAddress": None,
-        "nodeWalletBalance": None,
-        "clusterNames": None,
-        "formerClusterNames": None,
-        "state": None,
-        "clusterState": None,
-        "formerClusterState": None,
-        "clusterConnectivity": None,
-        "formerClusterConnectivity": None,
-        "rewardState": None,
-        "formerRewardState": None,
-        "rewardTrueCount": None,
-        "rewardFalseCount": None,
-        "clusterAssociationTime": None,
-        "formerClusterAssociationTime": None,
-        "clusterDissociationTime": None,
-        "formerClusterDissociationTime": None,
-        "nodeClusterSession": None,
-        "latestClusterSession": None,
-        "nodePeerCount": None,
-        "clusterPeerCount": None,
-        "formerClusterPeerCount": None,
-        "version": None,
-        "clusterVersion": None,
-        "latestVersion": latest_tessellation_version,
-        "cpuCount": None,
-        "diskSpaceTotal": None,
-        "diskSpaceFree": None,
-        "1mSystemLoadAverage": None,
-        "notify": False if requester is None else True,
-        "lastNotifiedTimestamp": None,
-        "timestampIndex": dt_start.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-        "formerTimestampIndex": None
-
-    }
-
-
 async def check(dask_client, bot, process_msg, requester, subscriber, port, layer, latest_tessellation_version: str,
                 history_dataframe, all_cluster_data: list[dict], dt_start, configuration: dict) -> tuple:
     process_msg = await discord.update_request_process_msg(process_msg, 2, None)
