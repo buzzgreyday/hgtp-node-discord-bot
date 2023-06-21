f8a1a63d47542e179af97a726302a5fd6d72fe31
Michael <micbr@pm.me>
2023-06-21 18:43:24 +0200

    Introduce FastAPI and SQL user sub

--- assets/code/database.py
+++ assets/code/database.py
@@ -1,38 +1,43 @@
 from sqlalchemy import Column, String, Integer, Float, DateTime, MetaData
 from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from fastapi import FastAPI, Depends
 from assets.code.schemas import User
 # from assets.code.schemas import SQLUserData
 
-engine = create_engine("sqlite:///assets/data/users/db/user.db", echo=True)
+engine = create_async_engine("sqlite+aiosqlite:///assets/data/users/db/user.db", connect_args={"check_same_thread": False})
 SQLBase = declarative_base()
 
-SQLBase.metadata.create_all(engine)
-SessionLocal = sessionmaker(engine)
+SessionLocal = async_sessionmaker(engine)
 
 api = FastAPI()
 
 
-def get_db():
+SQLBase.metadata.create_all(engine)
+
+
+async def get_db():
+    async with engine.begin as conn:
+        await conn.run_sync(SQLBase.metadata.create_all)
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
 
 
 @api.post("/user")
-def index(user: User, db: Session = Depends(get_db)):
+async def index(user: User, db: AsyncSession = Depends(get_db)):
     db_user = User.Create(user)
     db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    await db.commit()
+    await db.refresh(db_user)
+    return
 
 
 @api.get("/user")
-def get_users(db: Session = Depends(get_db)):
+async def get_users(db: AsyncSession = Depends(get_db)):
     users = db.query(User).all()
     return {"users": users}
