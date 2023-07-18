74e6b9acf1807a19f09618fd06c435c339d3489f
Michael <micbr@pm.me>
2023-07-18 09:38:35 +0200

    Node Data SQLBase

--- assets/src/database.py
+++ assets/src/database.py
@@ -64,10 +64,11 @@ class NodeData(SQLBase):
     former_cluster_dissociation_time: Mapped[float]
     former_cluster_name: Mapped[str]
     former_cluster_peer_count: Mapped[int]
+    former_cluster_state = Mapped[str]
     former_reward_state: Mapped[bool]
     former_timestamp_index: Mapped[datetime.datetime]
     ip: Mapped[str]
-    id: Mapped[str] = mapped_column(default=sqlalchemy.String, index=True, nullable=False, primary_key=True)
+    id: Mapped[str]
     last_notified_timestamp: Mapped[datetime.datetime]
     latest_cluster_session: Mapped[int]
     latest_version: Mapped[str]
@@ -84,7 +85,7 @@ class NodeData(SQLBase):
     reward_state: Mapped[bool]
     reward_true_count: Mapped[int]
     state: Mapped[str]
-    timestamp_index: Mapped[datetime.datetime]
+    timestamp_index: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime, index=True, nullable=False, primary_key=True)
     version: Mapped[str]
 
 
@@ -120,7 +121,54 @@ async def post_user(data: UserModel, db: AsyncSession = Depends(get_db)):
 
 @api.post("/data/create")
 async def post_data(data: NodeModel, db: AsyncSession = Depends(get_db)):
-    pass
+    data_dict = data.dict()
+    node_data = NodeData(**data_dict)
+    data_dict.update({
+        "name": NodeData.name,
+        "id": NodeData.id,
+        "ip": NodeData.ip,
+        "public_port": NodeData.public_port,
+        "layer": NodeData.layer,
+        "contact": NodeData.contact,
+        "cluster_association_time": NodeData.cluster_association_time,
+        "cpu_count": NodeData.cpu_count,
+        "one_m_system_load_average": NodeData.one_m_system_load_average,
+        "disk_space_free": NodeData.disk_space_free,
+        "disk_space_total": NodeData.disk_space_total,
+        "p2p_port": NodeData.p2p_port,
+        "wallet_address": NodeData.wallet_address,
+        "wallet_balance": NodeData.wallet_balance,
+        "cluster_name": NodeData.cluster_name,
+        "former_cluster_name": NodeData.former_cluster_name,
+        "state": NodeData.state,
+        "cluster_state": NodeData.cluster_state,
+        "former_cluster_state": NodeData.former_cluster_state,
+        "cluster_connectivity": NodeData.cluster_connectivity,
+        "former_cluster_connectivity": NodeData.former_cluster_connectivity,
+        "reward_state": NodeData.reward_state,
+        "former_reward_state": NodeData.former_reward_state,
+        "reward_true_count": NodeData.reward_true_count,
+        "reward_false_count": NodeData.reward_false_count,
+        "former_cluster_association_time": NodeData.former_cluster_association_time,
+        "cluster_dissociation_time": NodeData.cluster_dissociation_time,
+        "former_cluster_dissociation_time": NodeData.former_cluster_dissociation_time,
+        "node_cluster_session": NodeData.node_cluster_session,
+        "latest_cluster_session": NodeData.latest_cluster_session,
+        "node_peer_count": NodeData.node_peer_count,
+        "cluster_peer_count": NodeData.cluster_peer_count,
+        "former_cluster_peer_count": NodeData.former_cluster_peer_count,
+        "version": NodeData.version,
+        "cluster_version": NodeData.cluster_version,
+        "latest_version": NodeData.latest_version,
+        "notify": NodeData.notify,
+        "last_notified_timestamp": NodeData.last_notified_timestamp,
+        "timestamp_index": NodeData.timestamp_index,
+        "former_timestamp_index": NodeData.former_timestamp_index
+    })
+    db.add(node_data)
+    await db.commit()
+    await db.refresh(node_data)
+    return jsonable_encoder(data_dict)
 
 
 @api.get("/user")
@@ -156,6 +204,9 @@ async def get_contact_node_id(contact, db: AsyncSession = Depends(get_db)):
     results = await db.execute(select(User).where(User.contact == contact))
     nodes = results.scalars().all()
     return {contact: nodes}
+
+
+
 
 
 @api.get("/data/node/{ip}/{public_port}")
