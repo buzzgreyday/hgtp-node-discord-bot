88dac808858b3a3e62ae302437b88cea5f6a7328
Michael <micbr@pm.me>
2023-07-18 22:49:18 +0200

    Node Data SQLBase

--- assets/src/database.py
+++ assets/src/database.py
@@ -53,7 +53,13 @@ class NodeData(SQLBase):
     __tablename__ = "data"
 
     index: Mapped[int] = mapped_column(primary_key=True)
-    one_m_system_load_average: Mapped[Optional[float]] = mapped_column(nullable=True)
+    timestamp_index: Mapped[datetime.datetime]
+    name: Optional[str]
+    contact: Mapped[str]
+    ip: Mapped[str]
+    id: Mapped[str]
+    public_port: Mapped[int]
+    layer: Mapped[int]
     cluster_association_time: Mapped[Optional[float]] = mapped_column(nullable=True)
     cluster_connectivity: Mapped[Optional[str]] = mapped_column(nullable=True)
     cluster_dissociation_time: Mapped[Optional[float]] = mapped_column(nullable=True)
@@ -61,10 +67,23 @@ class NodeData(SQLBase):
     cluster_peer_count: Mapped[Optional[int]] = mapped_column(nullable=True)
     cluster_state: Mapped[Optional[str]] = mapped_column(nullable=True)
     cluster_version: Mapped[Optional[str]] = mapped_column(nullable=True)
-    contact: Mapped[str]
     cpu_count: Mapped[Optional[int]] = mapped_column(nullable=True)
     disk_space_free: Mapped[Optional[float]] = mapped_column(nullable=True)
     disk_space_total: Mapped[Optional[float]] = mapped_column(nullable=True)
+    one_m_system_load_average: Mapped[Optional[float]] = mapped_column(nullable=True)
+    latest_cluster_session: Mapped[Optional[int]] = mapped_column(nullable=True)
+    latest_version: Mapped[Optional[str]] = mapped_column(nullable=True)
+    node_cluster_session: Mapped[Optional[int]] = mapped_column(nullable=True)
+    node_peer_count: Mapped[Optional[int]] = mapped_column(nullable=True)
+    wallet_address: Mapped[Optional[str]] = mapped_column(nullable=True)
+    wallet_balance: Mapped[Optional[float]] = mapped_column(nullable=True)
+    notify: Mapped[Optional[bool]] = mapped_column(nullable=True)
+    p2p_port: Mapped[Optional[int]] = mapped_column(nullable=True)
+    reward_false_count: Mapped[Optional[int]] = mapped_column(nullable=True)
+    reward_state: Mapped[Optional[bool]] = mapped_column(nullable=True)
+    reward_true_count: Mapped[Optional[int]] = mapped_column(nullable=True)
+    state: Mapped[Optional[str]] = mapped_column(nullable=True)
+    version: Mapped[Optional[str]] = mapped_column(nullable=True)
     former_cluster_association_time: Mapped[Optional[float]] = mapped_column(nullable=True)
     former_cluster_connectivity: Mapped[Optional[str]] = mapped_column(nullable=True)
     former_cluster_dissociation_time: Mapped[Optional[float]] = mapped_column(nullable=True)
@@ -73,26 +92,8 @@ class NodeData(SQLBase):
     former_cluster_state: Mapped[Optional[str]] = mapped_column(nullable=True)
     former_reward_state: Mapped[Optional[bool]] = mapped_column(nullable=True)
     former_timestamp_index: Mapped[Optional[datetime.datetime]] = mapped_column(nullable=True)
-    ip: Mapped[str]
-    id: Mapped[str]
     last_notified_timestamp: Mapped[Optional[datetime.datetime]] = mapped_column(nullable=True)
-    latest_cluster_session: Mapped[Optional[int]] = mapped_column(nullable=True)
-    latest_version: Mapped[Optional[str]] = mapped_column(nullable=True)
-    layer: Mapped[Optional[int]] = mapped_column(nullable=True)
-    name: Mapped[Optional[str]] = mapped_column(nullable=True)
-    node_cluster_session: Mapped[Optional[int]] = mapped_column(nullable=True)
-    node_peer_count: Mapped[Optional[int]] = mapped_column(nullable=True)
-    wallet_address: Mapped[Optional[str]] = mapped_column(nullable=True)
-    wallet_balance: Mapped[Optional[float]] = mapped_column(nullable=True)
-    notify: Mapped[Optional[bool]] = mapped_column(nullable=True)
-    p2p_port: Mapped[Optional[int]] = mapped_column(nullable=True)
-    public_port: Mapped[int]
-    reward_false_count: Mapped[Optional[int]] = mapped_column(nullable=True)
-    reward_state: Mapped[Optional[bool]] = mapped_column(nullable=True)
-    reward_true_count: Mapped[Optional[int]] = mapped_column(nullable=True)
-    state: Mapped[Optional[str]] = mapped_column(nullable=True)
-    timestamp_index: Mapped[datetime.datetime]
-    version: Mapped[Optional[str]] = mapped_column(nullable=True)
+
 
 
 async def get_db() -> AsyncSession:
