6210f4a2b76a82b2802a552b2411cc403b42737b
Michael <micbr@pm.me>
2023-07-22 19:49:33 +0200

    fix new user node data merge data crash

--- assets/src/node.py
+++ assets/src/node.py
@@ -7,7 +7,8 @@ def merge_data(node_data: schemas.Node, cluster_data):
     if node_data.layer == cluster_data["layer"]:
         name = list(str(value) for value in
                     (node_data.former_cluster_name, node_data.former_cluster_name, node_data.last_known_cluster_name) if
-                    value is not None)[0]
+                    value is not None)
+        name = name[0] if name else None
         if name == cluster_data["name"]:
             node_data.latest_cluster_session = cluster_data["session"]
             node_data.cluster_version = cluster_data["version"]
