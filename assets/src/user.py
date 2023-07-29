f860ce06a6823fe0855213bd0058c364b0e049b9
Michael <micbr@pm.me>
2023-07-29 16:06:44 +0200

    I need to move the project folder

--- assets/src/user.py
+++ assets/src/user.py
@@ -12,21 +12,23 @@ IP_REGEX = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4]
 async def check(latest_tessellation_version, requester, all_cluster_data, dt_start, process_msg, _configuration) -> List[schemas.Node]:
     futures = []
     data = []
-    for lst in await locate_ids(requester, _configuration):
-        id_ = lst[0]
-        ip = lst[1]
-        port = lst[2]
-        layer = lst[3]
-        subscriber = await locate_node(_configuration, requester, id_, ip, port)
-        subscriber = pd.DataFrame(subscriber)
-        futures.append(asyncio.create_task(
-            node.check(bot, process_msg, requester, subscriber, port, layer,
-                       latest_tessellation_version, all_cluster_data, dt_start,
-                       _configuration)))
-    for async_process in futures:
-        d, process_msg = await async_process
-        data.append(d)
-    return data
+    ids = await locate_ids(requester, _configuration)
+    if ids is not None:
+        for lst in ids:
+            id_ = lst[0]
+            ip = lst[1]
+            port = lst[2]
+            layer = lst[3]
+            subscriber = await locate_node(_configuration, requester, id_, ip, port)
+            subscriber = pd.DataFrame(subscriber)
+            futures.append(asyncio.create_task(
+                node.check(bot, process_msg, requester, subscriber, port, layer,
+                           latest_tessellation_version, all_cluster_data, dt_start,
+                           _configuration)))
+        for async_process in futures:
+            d, process_msg = await async_process
+            data.append(d)
+        return data
 
 
 async def locate_ids(requester, _configuration):
