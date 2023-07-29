12c2d5a0d4f9ceb58792df73b9cb7836576d8c17
Michael <micbr@pm.me>
2023-07-29 16:37:34 +0200

    Add is None check and WORKING_DIR

--- assets/src/determine_module.py
+++ assets/src/determine_module.py
@@ -1,25 +1,28 @@
 import importlib.util
 from typing import List
+from pathlib import Path
 
 from aiofiles import os
 import sys
 
 from assets.src import schemas
 
+WORKING_DIR = f'{str(Path.home())}/bot'
+
 
 async def notify(data: List[schemas.Node], configuration):
     if data is not None:
         for idx, d in enumerate(data):
             if await os.path.exists(
-                    f"{configuration['file settings']['locations']['cluster modules']}/{d.cluster_name}.py"):
+                    f"{WORKING_DIR}/{configuration['file settings']['locations']['cluster modules']}/{d.cluster_name}.py"):
                 module = set_module(d.cluster_name, configuration)
                 data[idx] = module.mark_notify(d, configuration)
             elif await os.path.exists(
-                    f"{configuration['file settings']['locations']['cluster modules']}/{d.former_cluster_name}.py"):
+                    f"{WORKING_DIR}/{configuration['file settings']['locations']['cluster modules']}/{d.former_cluster_name}.py"):
                 module = set_module(d.former_cluster_name, configuration)
                 data[idx] = module.mark_notify(d, configuration)
             elif await os.path.exists(
-                    f"{configuration['file settings']['locations']['cluster modules']}/{d.last_known_cluster_name}.py"):
+                    f"{WORKING_DIR}/{configuration['file settings']['locations']['cluster modules']}/{d.last_known_cluster_name}.py"):
                 module = set_module(d.last_known_cluster_name, configuration)
                 data[idx] = module.mark_notify(d, configuration)
         return data
@@ -28,7 +31,7 @@ async def notify(data: List[schemas.Node], configuration):
 def set_module(cluster_name, configuration):
     if cluster_name is not None:
         spec = importlib.util.spec_from_file_location(f"{cluster_name}.build_embed",
-                                                      f"{configuration['file settings']['locations']['cluster modules']}/{cluster_name}.py")
+                                                      f"{WORKING_DIR}/{configuration['file settings']['locations']['cluster modules']}/{cluster_name}.py")
         module = importlib.util.module_from_spec(spec)
         sys.modules[f"{cluster_name}.build_embed"] = module
         spec.loader.exec_module(module)
