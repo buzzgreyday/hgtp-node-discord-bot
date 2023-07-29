f6e9fc6bfe65333e86ddf2f190996fe968aade13
Michael <micbr@pm.me>
2023-07-29 15:50:34 +0200

    Full path to locations

--- assets/src/config.py
+++ assets/src/config.py
@@ -1,18 +1,21 @@
 import aiofiles
 import yaml
+from pathlib import Path
 
 from assets.src import schemas
+
+WORKING_DIR = f'{str(Path.home())}/bot'
 
 
 async def update_config_with_latest_values(cluster: schemas.Cluster, configuration):
     """Update the config file with latest values"""
     if configuration["modules"][cluster.name][cluster.layer]["id"] != cluster.id:
         configuration["modules"][cluster.name][cluster.layer]["id"] = cluster.id
-        async with aiofiles.open("config.yml", "w") as file:
+        async with aiofiles.open(f"{WORKING_DIR}/config.yml", "w") as file:
             await file.write(yaml.dump(configuration))
 
 
 async def load():
     """Load configuration file"""
-    async with aiofiles.open('config.yml', 'r') as file:
+    async with aiofiles.open(f'{WORKING_DIR}/config.yml', 'r') as file:
         return yaml.safe_load(await file.read())
