ba22e511f4f06dc5d142a07b209b4c2e53e5532d
Michael <micbr@pm.me>
2023-07-30 20:21:20 +0200

    Fix spelling error in default Discord embed style

--- assets/src/preliminaries.py
+++ assets/src/preliminaries.py
@@ -9,6 +9,7 @@ async def latest_version_github(configuration):
     data = await api.safe_request(
         f"{configuration['tessellation']['github']['url']}/"
         f"{configuration['tessellation']['github']['releases']['latest']}", configuration)
+    # HANDLE THIS, WHEN DATA IS NONE
     vers = data["tag_name"][1:] if data["tag_name"] is not None else latest_version_github(configuration)
     return vers
 
