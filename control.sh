61a52e960c45e27177dfb820fd797709fbf8e67f
Michael <micbr@pm.me>
2023-07-30 19:53:23 +0200

    fixed swap and update in control.sh

--- control.sh
+++ control.sh
@@ -137,7 +137,8 @@ function main() {
   echo "[2] Stop bot"
   echo "[3] Change branch and update bot"
   echo "[4] Install bot"
-  echo "[5] Exit"
+  echo "[5] Update control script"
+  echo "[6] Exit"
   echo
   read -rp "Bot: Choose number " input
   if [ "$input" == 1 ]; then
@@ -148,8 +149,12 @@ function main() {
     update_bot
   elif [ "$input" == 4 ]; then
     install_bot
-  elif [ "$input" == 5 ]; then
+  elif [ "$input" == 4 ]; then
+    kill $$ && cp "$HOME/bot/control.sh" "$HOME" && bash "$HOME/control.sh"
+  elif [ "$input" == 6 ]; then
     exit 0
+  else
+    pass
   fi
 }
 
