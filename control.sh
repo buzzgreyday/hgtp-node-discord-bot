e403bf9038be0c44922bc6ab78a1179e83179f10
Michael <micbr@pm.me>
2023-07-30 11:10:44 +0200

    refactored control.sh

--- control.sh
+++ control.sh
@@ -114,6 +114,7 @@ function install_bot() {
   fi
   sudo add-apt-repository ppa:deadsnakes/ppa -y
   sudo apt update
+  sudo apt install -y pip
   sudo apt install -y python3.11
   sudo apt install -y python3.10-venv
   sudo apt install -y libcurl4-openssl-dev
