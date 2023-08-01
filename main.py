a528cf8cb8e5c209a51e8a1e37f3db3bf7e2426f
Michael <micbr@pm.me>
2023-08-01 11:11:00 +0200

    Change formulation

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ async def command_error(ctx, bot):
     embed = nextcord.Embed(title="Not a valid command".upper(),
                            color=nextcord.Color.orange())
     embed.add_field(name=f"\U00002328` {ctx.message.content}`",
-                    value=f"`ⓘ You didn't input a valid`",
+                    value=f"`ⓘ You didn't input a valid command`",
                     inline=False)
     embed.set_author(name=ctx.message.author,
                      icon_url=bot.get_user(
