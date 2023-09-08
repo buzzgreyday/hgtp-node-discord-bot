4dabda7d93c7fd6062f5a88f51929dfbfc9aa3ed
Michael <micbr@pm.me>
2023-09-09 00:56:50 +0200

    control.sh

--- control.sh
+++ control.sh
@@ -111,7 +111,7 @@ function update_bot() {
       git -C "$HOME/bot" checkout experimental
     fi
     start_venv
-    pip3.11 install -r "$HOME/bot/requirements.txt"
+    $HOME/venv/bin/pip3.11 install -r "$HOME/bot/requirements.txt"
   fi
   main
 }
