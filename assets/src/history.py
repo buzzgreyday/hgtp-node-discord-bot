562423c8d1e782823d93ccd79765b9501892b2c6
Michael <micbr@pm.me>
2023-09-10 13:13:13 +0200

    Fix timeout error asyncio

--- assets/src/history.py
+++ assets/src/history.py
@@ -11,13 +11,9 @@ from assets.src import schemas, api, database, exception
 
 async def node_data(node_data: schemas.Node, _configuration):
     """Get historic node data"""
-    while True:
-        data = await api.Request(f"http://127.0.0.1:8000/data/node/{node_data.ip}/{node_data.public_port}").json(_configuration)
-        if data is None:
-            logging.getLogger(__name__).error(f"history.py - localhost error: database probably timed out")
-            await asyncio.sleep(1)
-        else:
-            break
+    data = await api.Request(f"http://127.0.0.1:8000/data/node/{node_data.ip}/{node_data.public_port}").json(_configuration)
+    if data is None:
+        logging.getLogger(__name__).error(f"history.py - localhost error: data/node/{node_data.ip}/{node_data.public_port} returned {data}")
 
     if data is not None:
         data = schemas.Node(**data)
