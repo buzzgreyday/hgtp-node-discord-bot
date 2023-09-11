46ccc907db5e9980870a4917b5a39ed5ba1a06b9
Michael <micbr@pm.me>
2023-09-11 22:56:21 +0200

    Retry get historic node data from database

--- assets/src/api.py
+++ assets/src/api.py
@@ -69,14 +69,31 @@ async def safe_request(request_url: str, configuration: dict):
 
 async def get_user_ids(layer, requester, _configuration):
     """RETURNS A LIST/SET OF TUPLES CONTAINING ID, IP, PORT (PER LAYER)"""
-    if requester is None:
-        return await Request(f"http://127.0.0.1:8000/user/ids/layer/{layer}").json(_configuration)
+    try:
+        if requester is None:
+            data = await Request(f"http://127.0.0.1:8000/user/ids/layer/{layer}").json(_configuration)
+        else:
+            data = await Request(f"http://127.0.0.1:8000/user/ids/contact/{requester}/layer/{layer}").json(_configuration)
+    except asyncio.TimeoutError:
+        logging.getLogger(__name__).warning(
+            f"api.py - Localhost timeout while getting user id")
+        await asyncio.sleep(1)
     else:
-        return await Request(f"http://127.0.0.1:8000/user/ids/contact/{requester}/layer/{layer}").json(_configuration)
+        return data
+
 
 
 async def locate_node(_configuration, requester, id_, ip, port):
     """Locate every subscription where ID is id_
     return await dask_client.compute(subscriber_dataframe[subscriber_dataframe.id == id_])"""
-    return await Request(f"http://127.0.0.1:8000/user/ids/{id_}/{ip}/{port}").json(_configuration)
+    while True:
+        try:
+            data = await Request(f"http://127.0.0.1:8000/user/ids/{id_}/{ip}/{port}").json(_configuration)
+        except asyncio.TimeoutError:
+            logging.getLogger(__name__).warning(
+                f"api.py - Localhost timeout while locating node")
+            await asyncio.sleep(1)
+        else:
+            return data
+
 
