4f8c92c6840bb89ee695fc2f598d3d1a82c66a29
Michael <micbr@pm.me>
2023-09-12 20:03:57 +0200

    The problem seems to be with requesting sqlite3 async from different tasks. It locks or times out. Also, in history.py node_data; the first lines won't work we need loop to ensure we get the data, only if it doesn't exist, we can continue.

--- assets/src/database.py
+++ assets/src/database.py
@@ -123,7 +123,6 @@ async def post_user(data: UserModel, db: AsyncSession = Depends(get_db)):
     data_dict = data.dict()
     user = User(**data_dict)
     async with db_lock:
-
         result = await db.execute(select(User).where((User.ip == data.ip) & (User.public_port == data.public_port)))
         # You only need one result that matches
         result = result.fetchone()
@@ -145,75 +144,82 @@ async def post_data(data: NodeModel, db: AsyncSession = Depends(get_db)):
     data.index = next_index
     data_dict = data.dict()
     node_data = NodeData(**data_dict)
-    db.add(node_data)
-    await db.commit()
-    await db.refresh(node_data)
-    await db.close()
-    return jsonable_encoder(data_dict)
+    async with db_lock:
+        db.add(node_data)
+        await db.commit()
+        await db.refresh(node_data)
+        await db.close()
+        return jsonable_encoder(data_dict)
 
 
 
 @api.get("/user")
 async def get_users(db: AsyncSession = Depends(get_db)):
     """Returns a list of all user data"""
-    results = await db.execute(select(User))
-    await db.close()
-    users = results.scalars().all()
-    return {"users": users}
+    async with db_lock:
+        results = await db.execute(select(User))
+        await db.close()
+        users = results.scalars().all()
+        return {"users": users}
 
 
 @api.get("/user/ids/layer/{layer}")
 async def get_user_ids(layer: int, db: AsyncSession = Depends(get_db)):
     """INSTEAD RETURN A TUPLE CONTAINING ID, IP, PORT!!!! Returns a list of all user IDs currently subscribed"""
     list_of_tuples = []
-    results = await db.execute(select(User).where(User.layer == layer))
-    await db.close()
-    ids = results.scalars().all()
-    for values in ids:
-        list_of_tuples.append((values.id, values.ip, values.public_port))
-    # return list(set(ids))
-    return list_of_tuples
+    async with db_lock:
+        results = await db.execute(select(User).where(User.layer == layer))
+        await db.close()
+        ids = results.scalars().all()
+        for values in ids:
+            list_of_tuples.append((values.id, values.ip, values.public_port))
+        # return list(set(ids))
+        return list_of_tuples
 
 
 @api.get("/user/ids/{id_}/{ip}/{port}")
 async def get_nodes(id_: str, ip: str, port: int, db: AsyncSession = Depends(get_db)):
     """Return user by ID"""
-    results = await db.execute(select(User).where((User.id == id_) & (User.ip == ip) & (User.public_port == port)))
-    await db.close()
-    nodes = results.scalars().all()
-    return nodes
+    async with db_lock:
+        results = await db.execute(select(User).where((User.id == id_) & (User.ip == ip) & (User.public_port == port)))
+        await db.close()
+        nodes = results.scalars().all()
+        return nodes
 
 
 @api.get("/user/node/{ip}/{public_port}")
 async def get_node(ip: str, public_port: int, db: AsyncSession = Depends(get_db)):
     """Return user by IP and port"""
-    results = await db.execute(select(User).where((User.ip == ip) & (User.public_port == public_port)))
-    await db.close()
-    node = results.scalars().all()
-    return {"node": node}
+    async with db_lock:
+        results = await db.execute(select(User).where((User.ip == ip) & (User.public_port == public_port)))
+        await db.close()
+        node = results.scalars().all()
+        return {"node": node}
 
 
 @api.get("/user/ids/contact/{contact}/layer/{layer}")
 async def get_contact_node_id(contact, layer, db: AsyncSession = Depends(get_db)):
     """INSTEAD RETURN A TUPLE CONTAINING ID, IP, PORT!!!! Return user by contact"""
     list_of_tuples = []
-    results = await db.execute(select(User).where((User.contact == contact) & (User.layer == layer)))
-    await db.close()
-    ids = results.scalars().all()
-    for values in ids:
-        list_of_tuples.append((values.id, values.ip, values.public_port, values.layer))
-    # return list(set(ids))
-    return list_of_tuples
+    async with db_lock:
+        results = await db.execute(select(User).where((User.contact == contact) & (User.layer == layer)))
+        await db.close()
+        ids = results.scalars().all()
+        for values in ids:
+            list_of_tuples.append((values.id, values.ip, values.public_port, values.layer))
+        # return list(set(ids))
+        return list_of_tuples
 
 
 @api.get("/data/node/{ip}/{public_port}")
 async def get_node_data(ip, public_port, db: AsyncSession = Depends(get_db)):
     """Return latest node data fetched via automatic check by IP and port"""
-    results = await db.execute(
-                    select(NodeData)
-                    .where((NodeData.ip == ip) & (NodeData.public_port == public_port))
-                    .order_by(NodeData.timestamp_index.desc())
-                    .limit(1))
-    await db.close()
-    node = results.scalar_one_or_none()
-    return node
+    async with db_lock:
+        results = await db.execute(
+                        select(NodeData)
+                        .where((NodeData.ip == ip) & (NodeData.public_port == public_port))
+                        .order_by(NodeData.timestamp_index.desc())
+                        .limit(1))
+        await db.close()
+        node = results.scalar_one_or_none()
+        return node
