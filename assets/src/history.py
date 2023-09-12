beee11847c6bbbf4726e13a4700235e374004184
Michael <micbr@pm.me>
2023-09-12 20:01:40 +0200

    The problem seems to be with requesting sqlite3 async from different tasks. It locks or times out. Also, in history.py node_data; the first lines won't work we need loop to ensure we get the data, only if it doesn't exist, we can continue.

--- assets/src/history.py
+++ assets/src/history.py
@@ -15,11 +15,14 @@ async def node_data(node_data: schemas.Node, _configuration):
     #
     # This here is the biggest problem it seems
     # The problem seems to be with requesting sqlite3 async from different tasks. It locks or times out.
-    """loop infinite here"""
-    data = await api.safe_request(f"http://127.0.0.1:8000/data/node/{node_data.ip}/{node_data.public_port}", _configuration)
-    # This section won't do. We need to get the historic data; the first lines won't work we need loop to ensure we get the data, only if it doesn't exist, we can continue.
-    if data is None:
-        logging.getLogger(__name__).warning(f"history.py - localhost error: data/node/{node_data.ip}/{node_data.public_port} returned {data}")
+    while True:
+        data = await api.safe_request(f"http://127.0.0.1:8000/data/node/{node_data.ip}/{node_data.public_port}", _configuration)
+        # This section won't do. We need to get the historic data; the first lines won't work we need loop to ensure we get the data, only if it doesn't exist, we can continue.
+        if data:
+            break
+        else:
+            logging.getLogger(__name__).warning(f"history.py - localhost error: data/node/{node_data.ip}/{node_data.public_port} returned {data}")
+            await asyncio.sleep(1)
 
     if data is not None:
         data = schemas.Node(**data)
