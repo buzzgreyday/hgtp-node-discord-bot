2eb7c612b1e4efdf5f55bbd6317db3c81319e743
Michael <micbr@pm.me>
2023-09-25 12:54:39 +0200

    Refactoring

--- assets/src/database/crud.py
+++ assets/src/database/crud.py
@@ -13,23 +13,22 @@ session = async_sessionmaker(bind=engine, expire_on_commit=False)
 class CRUD:
 
     @staticmethod
-    async def get_next_index(Model, async_session: async_sessionmaker[AsyncSession]) -> int:
+    async def get_next_index(Model, session) -> int:
         """Fetch the last assigned index from the separate table"""
-        async with async_session() as session:
-            statement = select(Model.index).order_by(Model.index.desc()).limit(1)
-            result = await session.execute(statement)
-            last_index = result.scalar_one_or_none()
-            return 0 if last_index is None else last_index + 1
+        statement = select(Model.index).order_by(Model.index.desc()).limit(1)
+        result = await session.execute(statement)
+        last_index = result.scalar_one_or_none()
+        return 0 if last_index is None else last_index + 1
 
-    async def post_user(self, data, async_session: async_sessionmaker[AsyncSession]):
+    async def post_user(self, data: User, async_session: async_sessionmaker[AsyncSession]):
         """Creates a new user subscription"""
-        next_index = await CRUD().get_next_index(User, async_sessionmaker[AsyncSession])
-        data.index = next_index
-        data.date = datetime.utcnow()
-        data_dict = data.dict()
-        user = User(**data_dict)
-        # async with db_lock:
         async with async_session() as session:
+            next_index = await CRUD().get_next_index(User, session)
+            data.index = next_index
+            data.date = datetime.utcnow()
+            data_dict = data.dict()
+            user = User(**data_dict)
+            # async with db_lock:
             statement = select(User).where((User.ip == data.ip) & (User.public_port == data.public_port))
             result = await session.execute(statement)
             # You only need one result that matches
@@ -42,5 +41,26 @@ class CRUD:
                     f"crud.py - The user {data.name} ({data.ip}:{data.public_port}) was added to the list of subscribers")
                 session.add(user)
                 await session.commit()
+        return jsonable_encoder(data_dict)
+
+    async def post_data(self, data: NodeData, async_session: async_sessionmaker[AsyncSession]):
+        """Inserts node data from automatic check into database file"""
+        next_index = await get_next_index(NodeData, db)
+        data.index = next_index
+        data_dict = data.dict()
+        node_data = NodeData(**data_dict)
+        # async with db_lock:
+        db.add(node_data)
+        while True:
+            try:
+                await db.commit()
+            except (sqlalchemy.exc.OperationalError, sqlite3.OperationalError):
+                logging.getLogger(__name__).info(
+                    f"database.py - Operational error while posting node data  {data.name} ({data.ip}:{data.public_port}, {data.last_known_cluster_name})")
+                await asyncio.sleep(1)
+            else:
+                break
+        await db.refresh(node_data)
+        await db.close()
         return jsonable_encoder(data_dict)
 
