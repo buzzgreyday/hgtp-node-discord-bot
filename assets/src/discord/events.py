ae605785ee86d4187fb909ac6b66571b0e24f57a
Michael <micbr@pm.me>
2023-09-26 12:17:16 +0200

    Refactoring

--- assets/src/discord/events.py
+++ assets/src/discord/events.py
@@ -16,20 +16,20 @@ async def on_message(message):
     ctx = await bot.get_context(message)
     if ctx.valid:
         logging.getLogger(__name__).info(
-            f"main.py - Command received from {ctx.message.author} in {ctx.message.channel}")
+            f"events.py - Command received from {ctx.message.author} in {ctx.message.channel}")
         await bot.process_commands(message)
     else:
         if ctx.message.channel.id in (977357753947402281, 974431346850140204, 1030007676257710080, 1134396471639277648):
             # IGNORE INTERPRETING MESSAGES IN THESE CHANNELS AS COMMANDS
             logging.getLogger(__name__).info(
-                f"main.py - Received a command in a non-command channel")
+                f"events.py - Received a command in a non-command channel")
         elif ctx.message.channel.id == 1136386732628115636:
             logging.getLogger(__name__).info(
-                f"main.py - Received a message in the verify channel")
+                f"events.py - Received a message in the verify channel")
             await discord.delete_message(ctx)
         else:
             logging.getLogger(__name__).info(
-                f"main.py - Received an unknown command from {ctx.message.author} in {ctx.message.channel}")
+                f"events.py - Received an unknown command from {ctx.message.author} in {ctx.message.channel}")
             await message.add_reaction("\U0000274C")
             if not isinstance(ctx.message.channel, nextcord.DMChannel):
                 await message.delete(delay=3)
@@ -39,4 +39,4 @@ async def on_message(message):
 @bot.event
 async def on_ready():
     """Prints a message to the logs when a connection to Discord is established (bot is running)"""
-    logging.getLogger(__name__).info(f"main.py - Discord connection established")
+    logging.getLogger(__name__).info(f"events.py - Discord connection established")
