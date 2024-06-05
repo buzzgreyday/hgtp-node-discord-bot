1ca26823e3690dee6b881df3a2ee275b28d3c108
michael <micbr@protonmail.com>
2024-06-05 14:54:13 +0200

    One upped html version to 0.0.10

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def main():
 
     version_manager = preliminaries.VersionManager(_configuration)
 
-    bot.load_extension("assets.src.discord.commands")
+    # bot.load_extension("assets.src.discord.commands")
     bot.load_extension("assets.src.discord.events")
 
     bot.loop.create_task(main_loop(version_manager, _configuration))
@@ -158,10 +158,10 @@ def main():
     uvicorn_thread.start()
     rewards_thread = threading.Thread(target=start_rewards_coroutine, args=(_configuration,))
     rewards_thread.start()
-    stats_thread = threading.Thread(
-        target=start_stats_coroutine, args=(_configuration,)
-    )
-    stats_thread.start()
+    # stats_thread = threading.Thread(
+    #     target=start_stats_coroutine, args=(_configuration,)
+    # )
+    # stats_thread.start()
 
     while True:
         try:
