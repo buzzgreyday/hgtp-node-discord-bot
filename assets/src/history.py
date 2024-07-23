09e5b45c61f83a3219f6dd2ad421bc796261fcbe
michael <micbr@protonmail.com>
2024-07-23 15:33:25 +0200

    Ready for release

--- assets/src/history.py
+++ assets/src/history.py
@@ -28,7 +28,7 @@ async def node_data(session, requester, node_data: schemas.Node, _configuration)
             client_exceptions.ServerDisconnectedError,
         ):
             logging.getLogger("app").warning(
-                f"history.py - localhost error - status {resp_status}: data/node/{node_data.ip}/{node_data.public_port} ({localhost_error_retry}/{2}): {traceback.format_exc()}"
+                f"history.py - localhost error: data/node/{node_data.ip}/{node_data.public_port} ({localhost_error_retry}/{2}): {traceback.format_exc()}"
             )
             # Did the user unsubscribe
             if localhost_error_retry <= 2:
