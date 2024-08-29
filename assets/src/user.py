28bd7234a3781b3b458feeed3ec3a2ff0f8d86de
michael <micbr@protonmail.com>
2024-08-29 08:44:28 +0200

    Refactor

--- assets/src/user.py
+++ assets/src/user.py
@@ -4,69 +4,10 @@ from typing import List
 
 import pandas as pd
 
-from assets.src import schemas, database, api, history, cluster, determine_module
+from assets.src import schemas, database, api, history, cluster, determine_module, check
 from assets.src.database import database, models
 
 IP_REGEX = r'^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$'
-
-
-async def node_status_check(
-        session,
-        subscriber,
-        cluster_data: schemas.Cluster,
-        version_manager,
-        configuration: dict,
-) -> schemas.Node:
-    node_data = schemas.Node(
-        name=subscriber.name.values[0],
-        discord=subscriber.discord.values[0],
-        mail=subscriber.mail.values[0],
-        phone=subscriber.phone.values[0],
-        ip=subscriber.ip.values[0],
-        layer=subscriber.layer.values[0],
-        public_port=subscriber.public_port.values[0],
-        id=subscriber.id.values[0],
-        wallet_address=subscriber.wallet.values[0],
-        latest_version=version_manager.get_version(),
-        notify=False,
-        timestamp_index=datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=None),
-    )
-    node_data = await history.node_data(session, None, node_data, configuration)
-    found_in_cluster, cluster_data = cluster.locate_node(node_data, cluster_data)
-    node_data = cluster.merge_data(node_data, found_in_cluster, cluster_data)
-    node_data = await cluster.get_module_data(session, node_data, configuration)
-    # You need to check rewards here, if association is made but cluster is down!
-    # The way to do this is to check add addresses for cluster, even if cluster is down
-    # Think I did this now
-    if (
-            node_data.cluster_name is not None
-            and cluster_data is not None
-            and configuration["modules"][node_data.cluster_name][node_data.layer]["rewards"]
-    ):
-        node_data = determine_module.set_module(
-            node_data.cluster_name, configuration
-        ).check_rewards(node_data, cluster_data)
-    elif (
-            node_data.former_cluster_name is not None
-            and cluster_data is not None
-            and configuration["modules"][node_data.former_cluster_name][node_data.layer][
-                "rewards"
-            ]
-    ):
-        node_data = determine_module.set_module(
-            node_data.former_cluster_name, configuration
-        ).check_rewards(node_data, cluster_data)
-    elif (
-            node_data.last_known_cluster_name is not None
-            and cluster_data is not None
-            and configuration["modules"][node_data.last_known_cluster_name][
-                node_data.layer
-            ]["rewards"]
-    ):
-        node_data = determine_module.set_module(
-            node_data.last_known_cluster_name, configuration
-        ).check_rewards(node_data, cluster_data)
-    return node_data
 
 
 async def process_node_data_per_user(
@@ -80,14 +21,14 @@ async def process_node_data_per_user(
             id_, ip, port = lst
             while True:
                 subscriber = await api.locate_node(
-                    session, _configuration, None, id_, ip, port
+                    session=session, node_id=id_, ip=ip, port=port
                 )
                 if subscriber:
                     break
             subscriber = pd.DataFrame(subscriber)
             futures.append(
                 asyncio.create_task(
-                    node_status_check(
+                    check.node_status(
                         session,
                         subscriber,
                         cluster_data,
