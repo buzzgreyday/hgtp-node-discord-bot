7af5998f8ce99bf9502bfa71964383fe66198557
michael <micbr@protonmail.com>
2024-08-29 08:07:35 +0200

    Hotfixes

--- assets/src/user.py
+++ assets/src/user.py
@@ -29,7 +29,7 @@ async def node_status_check(
         wallet_address=subscriber.wallet.values[0],
         latest_version=version_manager.get_version(),
         notify=False,
-        timestamp_index=dt.datetime.now(datetime.timezone.utc),
+        timestamp_index=dt.datetime.now(datetime.timezone.utc).replace(tzinfo=None),
     )
     node_data = await history.node_data(session, None, node_data, configuration)
     found_in_cluster, cluster_data = cluster.locate_node(node_data, cluster_data)
