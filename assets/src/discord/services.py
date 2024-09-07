db0ca2cafa47d3a4ad0f60a27b4631621ca2333e
michael <micbr@protonmail.com>
2024-09-07 08:09:57 +0200

    Add test server compatibility

--- assets/src/discord/services.py
+++ assets/src/discord/services.py
@@ -5,12 +5,12 @@ import nextcord
 from nextcord.ext import commands
 
 load_dotenv()
+dev_env = os.getenv("NODEBOT_DEV_ENV")
 
 """LOAD DISCORD SERVER TOKEN FROM ENVIRONMENT"""
-discord_token = getenv("DISCORD_TOKEN")
+discord_token = getenv("DISCORD_TOKEN") if not dev_env else getenv("DISCORD_DEV_TOKEN")
 
 """GUILD CONFIG"""
-dev_env = os.getenv("NODEBOT_DEV_ENV")
 NODEBOT_GUILD = 974431346850140201
 NODEBOT_DEV_GUILD = 1281616185170853960
 guild_id = [int(NODEBOT_DEV_GUILD)] if dev_env else [int(NODEBOT_GUILD)]
