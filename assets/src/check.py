04e6b5780fd165f4f0f6bea18db9dc9c77167ce7
michael <micbr@protonmail.com>
2024-09-14 22:00:50 +0200

    Fixed requests

--- assets/src/check.py
+++ assets/src/check.py
@@ -144,7 +144,6 @@ async def request(session, process_msg, layer, requester, _configuration):
     await bot.wait_until_ready()
 
     if ids:
-        print("Got ids")
         version_manager = preliminaries.VersionManager(_configuration)
         process_msg = await messages.update_request_process_msg(process_msg, 2)
         for lst in ids:
@@ -154,7 +153,6 @@ async def request(session, process_msg, layer, requester, _configuration):
                 subscriber = await req.locate_node(
                     node_id=id_, ip=ip, port=port
                 )
-                print("Got subscriber")
                 if subscriber:
                     break
 
